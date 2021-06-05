21e95ea80c4fc6a822064b56d2e34457d76e3460
solitaryzhu <solitaryzhu@163.com>
2021-06-05 16:29:35 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf ./cpp_code && mkdir ./cpp_code
+if [ $? -ne 0 ];then
+    echo "error: code1"
+    exit -1
+fi
+rm -rf ./python_out && mkdir ./python_out
+if [ $? -ne 0 ];then
+    echo "error : code2"
+    exit -1
+fi
+rm -rf ./java_out && mkdir ./java_out
+if [ $? -ne 0 ];then
+    echo "error : code2"
+    exit -1
+fi
+protoc --proto_path=. --cpp_out=./cpp_code --python_out=./python_out --java_out=./java_out $(find . -name *.proto)
+
+echo "done"
